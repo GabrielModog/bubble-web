ce97a7cd59ec6176a3bea3ae3894ab88b1b99d09
GabrielModog <criativo.modog@gmail.com>
2024-04-10 00:51:34 -0300

    [feat] Add navbar and user dropdown

---
+++ components/Navbar.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Image from "next/image";
+
+import BubbleTextImage from "../public/bubble-logotipo.svg";
+import BubbleMobileImage from "../public/bubble-logo.svg";
+
+import { Button } from "./ui/button";
+import { ThemeToggle } from "./theme/ThemeToggle";
+import { UserDropdown } from "./user/UserDropdown";
+
+export async function Navbar() {
+  const { getUser } = getKindeServerSession();
+
+  const user = await getUser();
+
+  return (
+    <nav className="h-[10vh] w-full flex items-center border-b px-5 lg:px-14 justify-between">
+      <Link href="/" className="flex items-center gap-x-3">
+        <Image
+          src={BubbleMobileImage}
+          alt="Bubble Mobile Icon"
+          className="h-10 w-fit"
+        />
+        <Image
+          src={BubbleTextImage}
+          alt="Bubble Desktop"
+          className="h-9 w-fit hidden lg:block"
+        />
+      </Link>
+
+      <div className="flex items-center gap-x-3">
+        <ThemeToggle />
+        {user ? (
+          <UserDropdown userImage={user.picture} />
+        ) : (
+          <>
+            <Button variant="secondary" asChild>
+              <RegisterLink>Sign Up</RegisterLink>
+            </Button>
+            <Button asChild>
+              <LoginLink>Login</LoginLink>
+            </Button>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+}
---
+++ components/user/UserDropdown.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { MenuIcon } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+interface UserDropdownProps {
+  userImage: string | null;
+}
+
+const DEFAULT_USER_IMAGE = "https://i.pravatar.cc/300";
+
+export function UserDropdown(props: UserDropdownProps) {
+  const { userImage = DEFAULT_USER_IMAGE } = props;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="rounded-full border p-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
+          <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
+          <img
+            src={userImage ?? DEFAULT_USER_IMAGE}
+            alt="user avatar"
+            className="rounded-full size-8 hidden lg:block"
+          />
+        </div>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" className="w-[200px]">
+        <DropdownMenuItem>
+          <Link href="/r/create">Create a Bubble</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href="/create">Create Blob</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href="/settings">Settings</Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogoutLink>Logout</LogoutLink>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
